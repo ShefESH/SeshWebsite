af1d259dcf650206e3ecb21ff7ec59ecc8f137e6
Sohyun <sohyun.x.park@disney.com>
2020-09-20 15:12:09 +0100

    nav menu highlight fix

--- assets/js/navmenu.js
+++ assets/js/navmenu.js
@@ -2,23 +2,28 @@ function setNavmenuColor(document) {
     var url = window.location.href;
 
     if (url.indexOf("sessions") > -1) {
-        let myElement = document.querySelector("#sessions");
-        myElement.style.backgroundColor = "#1c1e29";
+        let element = document.querySelector("#sessions");
+        setStyle(element);
     }
     else if (url.indexOf("committee") > -1) {
-        let myElement = document.querySelector("#committee");
-        myElement.style.backgroundColor = "#1c1e29";
+        let element = document.querySelector("#committee");
+        setStyle(element);
     }
     else if (url.indexOf("contact") > -1) {
-        let myElement = document.querySelector("#contact");
-        myElement.style.backgroundColor = "#1c1e29";
+        let element = document.querySelector("#contact");
+        setStyle(element);
     }
     else if (url.indexOf("careers") > -1) {
-        let myElement = document.querySelector("#careers");
-        myElement.style.backgroundColor = "#1c1e29";
+        let element = document.querySelector("#careers");
+        setStyle(element);
     }
     else if (url.indexOf("wiki") > -1) {
-        let myElement = document.querySelector("#wiki");
-        myElement.style.backgroundColor = "#1c1e29";
+        let element = document.querySelector("#wiki");
+        setStyle(element);
     }
+}
+
+function setStyle(element) {
+    element.style.backgroundColor = "#1c1e29";
+    element.style.borderRadius = "5px";
 }
